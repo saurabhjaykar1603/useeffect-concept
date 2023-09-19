4343f4cb5814706353d510d2d334d1d31fbc9008
saurabh jaykar <saurabh@kali.kali.org>
2023-09-19 13:42:37 +0530

    feat : usect added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const increment = () =>{
-    setCount(count + 1)
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
-  } 
-  const decrement = () =>{
-    setCount(count - 1)
-  }
-return (
+  // useEffect hook have two parameters
+  // 1. callback function
+  // 2. array of dependencies
+  
+  useEffect(() => {
+    console.log(`new value ${count}`);
+  }, [count]);
+
+  return (
     <>
-      <h1 className='Count'>Count :{count}</h1>
-      <div className='d-flex justify-center'>
+      <h1 className="Count">Count :{count}</h1>
+      <div className="d-flex justify-center">
         <button onClick={decrement}>Dec</button>
-        <button onClick={increment} >Inc</button>
+        <button onClick={increment}>Inc</button>
       </div>
     </>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
